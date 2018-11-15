10d0c728c90b346718a7a271a328aad6492a2853
alanna.botscharow <alanna.botscharow@CLC-LAB209-18.TCSD.tooelesd.org>
2018-11-15 14:14:37 -0700

    WIP

---
+++ src/AlannaBotscharowChapter5Pr2/chapter5Program2.java
@@ -0,0 +1,166 @@
+package AlannaBotscharowChapter5Pr2;
+import java.util.Scanner; 
+/* 
+ * Alanna Botscharow
+ * 11/18
+ */
+public class chapter5Program2 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		// Prompt user for input of a character. 
+		
+			System.out.println("Please enter a character: ");
+		
+		//Get user input
+			//declare variables 
+		
+		char ch = input.next().charAt(0); 
+		
+		//If statements
+			//Display 
+
+		//0-9
+		if (ch == '0') {
+			System.out.println("The unicode for the character 0 is \u0030"); 
+			
+		}
+		else if (ch == '1') {
+			System.out.println("The unicode for the character 1 is \u0031"); 
+		}
+		else if (ch == '2') {
+			System.out.println("The unicode for the character 2 is \u0031"); 
+		}
+		else if (ch == '3') {
+			System.out.println("The unicode for the character 3 is \u0032"); 
+		}
+		else if (ch == '4') {
+			System.out.println("The unicode for the character 4 is \u0034"); 
+		}
+		else if (ch == '5') {
+			System.out.println("The unicode for the character 5 is \u0035"); 
+		}
+		else if (ch == '6') {
+			System.out.println("The unicode for the character 6 is \u0036"); 
+		}
+		else if (ch == '7') {
+			System.out.println("The unicode for the character 7 is \u0037"); 
+		}
+		else if (ch == '8') {
+			System.out.println("The unicode for the character 8 is \u0038"); 
+		}
+		else if (ch == '9') {
+			System.out.println("The unicode for the character 9 is \u0039"); 
+		}
+		
+			
+			//a-z
+			
+		
+		else if (ch == 'a') {
+			System.out.println("The unicode for the character a is \u0061"); 
+		}
+		else if (ch == 'b') {
+			System.out.println("The unicode for the character a is \u0061"); 
+		}
+		else if (ch == 'c') {
+			System.out.println("The unicode for the character a is \u0061"); 
+		}
+		else if (ch == 'd') {
+			System.out.println("The unicode for the character a is \u0061"); 
+		}
+		else if (ch == 'e') {
+			System.out.println("The unicode for the character a is \u0061"); 
+		}
+		else if (ch == 'f') {
+			System.out.println("The unicode for the character a is \u0061"); 
+		}
+		else if (ch == 'g') {
+			System.out.println("The unicode for the character a is \u0061"); 
+		}
+		else if (ch == 'h') {
+			System.out.println("The unicode for the character a is \u0061"); 
+		}
+		else if (ch == 'i') {
+			System.out.println("The unicode for the character a is \u0061"); 
+		}
+		else if (ch == 'j') {
+			System.out.println("The unicode for the character a is \u0061"); 
+		}
+		else if (ch == 'k') {
+			System.out.println("The unicode for the character a is \u0061"); 
+		}
+		else if (ch == 'l') {
+			System.out.println("The unicode for the character a is \u0061"); 
+		}
+		else if (ch == 'm') {
+			System.out.println("The unicode for the character a is \u0061"); 
+		}
+		else if (ch == 'n') {
+			System.out.println("The unicode for the character a is \u0061"); 
+		}
+		else if (ch == 'o') {
+			System.out.println("The unicode for the character a is \u0061"); 
+		}
+		else if (ch == 'p') {
+			System.out.println("The unicode for the character a is \u0061"); 
+		}
+		else if (ch == 'q') {
+			System.out.println("The unicode for the character a is \u0061"); 
+		}
+		else if (ch == 'r') {
+			System.out.println("The unicode for the character a is \u0061"); 
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		/*
+		 * 			char chara = '\u0061';
+					char charb = '\u0062';
+					char charc = '\u0063';
+					char chard = '\u0064';
+					char chare = '\u0065';
+					char charf = '\u0066';
+					char charg = '\u0067';
+					char charh = '\u0068';
+					char chari = '\u0069';
+					char charj = '\u006A';
+					char chark = '\u006B';
+					char charl = '\u006C';
+					char charm = '\u006D';
+					char charn = '\u006E';
+					char charo = '\u006F';
+					char charp = '\u0070';
+					char charq = '\u0071';
+					char charr = '\u0072';
+					char chars = '\u0073';
+					char chart = '\u0074';
+					char charu = '\u0075';
+					char charv = '\u0076';
+					char charw = '\u0077';
+					char charx = '\u0078';
+					char chary = '\u0079';
+					char charz = '\u007A';
+		 */
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
